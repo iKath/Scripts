2183f1ee50efd224baee4a132bb6c7df3fc15b04
iKath <42527047+iKath@users.noreply.github.com>
2022-12-08 22:57:33 +0800

    Update FileBall.js

--- FileBall.js
+++ FileBall.js
@@ -18,7 +18,7 @@
 [rewrite_local]
   
 # ～ FileBall解锁会员权限（2022-12-08）@ddgksf2013
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/*) url script-echo-response FileBallProCrack.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/*) url script-echo-response https://raw.githubusercontent.com/iKath/Scripts/main/FileBall.js
 ^https?:\/\/firebaseremoteconfig\.googleapis\.com\/v\d\/projects\/filebox url script-echo-response https://raw.githubusercontent.com/iKath/Scripts/main/FileBall.js
 
 [mitm] 
